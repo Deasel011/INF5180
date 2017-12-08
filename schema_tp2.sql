1cb14f1b537492beda03d430c3f5d82bc9cec399
Philippe Deslongchamps <philippe.deslongchamps@gmail.com>
2017-12-07 20:00:42 -0500

    update shema tp2

--- schema_tp2.sql
+++ schema_tp2.sql
@@ -1,8 +1,15 @@
+-- noinspection SqlDialectInspectionForFile
+
+-- noinspection SqlNoDataSourceInspectionForFile
+
+set echo on;
+set serveroutput on;
+
 CREATE TABLE Conference
 (
-	titre VARACHAR(50) NOT NULL,
+	titre VARCHAR(50) NOT NULL,
 	acronyme VARCHAR(10) NOT NULL,
-	frequence INTEGER() NOT NULL,
+	frequence INTEGER NOT NULL,
 	CONSTRAINT acronyme_unique UNIQUE(acronyme),
 	CONSTRAINT conference_pk PRIMARY KEY (titre)
 );
@@ -14,80 +21,21 @@ CREATE TABLE Edition
 	venue VARCHAR(50) NOT NULL,
 	dateDebut DATE NOT NULL,
 	dateFin DATE NOT NULL,
-	CONSTRAINT edition_pk PRIMARY KEY (idEdition)
-	CONSTRAINT editiionConference_fk FOREIGN KEY (titre)
-		REFERENCE Conference(titre),
+	CONSTRAINT edition_pk PRIMARY KEY (idEdition),
+	CONSTRAINT editiionConference_fk FOREIGN KEY (titre) REFERENCES Conference(titre),
 	CONSTRAINT date_check CHECK (dateDebut <= dateFin)
 );
 
 CREATE TABLE Track
 (
-	idTrack NUMBER  NOT NULL,
-	idEdition NUMBER NOT NULL,
+	idTrack INTEGER NOT NULL,
+	idEdition INTEGER NOT NULL,
 	titre VARCHAR(50) NOT NULL, 
 	nbPapiersMin NUMBER NOT NULL,
 	nbPapiersMax NUMBER NOT NULL,
 	CONSTRAINT track_id PRIMARY KEY (idTrack),
-	CONSTRAINT trackEdition_fk FOREIGN KEY (idEdition)à
-		REFERENCE Edition(idEdition)
-	CONSTRAINT nbPapiers_check CHECK (nbPapiersMin < nbPapiersMax)
-
-);
-
-CREATE TABLE CoPresident
-(
-	idChercheur NUMBER NOT NULL,
-	idComiteRelecture NUMBER NOT NULL,
-	CONSTRAINT copresident_pk PRIMARY KEY (idChercheur, idComiteRelecture),
-	CONSTRAINT chercheur_fk FOREIGN KEY (idChercheur)
-		REFERENCE Chercheur(idChercheur)
-	CONSTRAINT CoPresComiteRelecture_fk FOREIGN KEY (idComiteRelecture)
-		REFERENCE Comiterelecture(idComiteRelecture)
-);
-
-CREATE TABLE ComiteRelecture
-(
-	idComiteRelecture NUMBER NOT NULL,
-	idTrack NUMBER NOT NULL,
-	CONSTRAINT idTrack_unique UNIQUE(idTrack),
-	CONSTRAINT comiteRelecture_pk PRIMARY KEY (idComiteRelecture),
-	CONSTRAINT ComiteTrack_fk FOREIGN KEY (idTrack)
-		REFERENCE Track(idTrack)
-);
-
-CREATE TABLE TrackASujet
-(
-	idTrack NUMBER NOT NULL,
-	motClef VARCHAR(30) NOT NULL,
-	CONSTRAINT TrackASujet_pk PRIMARY KEY (idTrack, motClef),
-	CONSTRAINT TrackASujetTrack_fk FOREIGN KEY (idTrack)
-		REFERENCE Track(idTrack)
-	CONSTRAINT TrackASujetS	ujet_fk FOREIGN KEY (motClef)
-		REFERENCE Sujet(motClef)
-);
-
-CREATE TABLE Membre
-(
-	idChercheur NUMBER NOT NULL,
-	idComiteRelecture NUMBER NOT NULL,
-	CONSTRAINT membre_pk PRIMARY KEY (idChercheur, idComiteRelecture),
-	CONSTRAINT membreChercheur_fk FOREIGN KEY (idChercheur)
-		REFERENCE Chercheur(idChercheur)
-	CONSTRAINT membreComite_fk FOREIGN KEY (idComiteRelecture)
-		REFERENCE ComiteRelecture(idComiteRelecture)
-);
-
-CREATE TABLE Soumission
-(
-	noSoumission NUMBER NOT NULL,
-	titre VARCHAR(50) NOT NULL,
-	resume VARCHAR(200) NOT NULL,
-	corps BLOB NOT NULL,
-	idTrack NUMBER NOT NULL,
-	CONSTRAINT soumission_pk PRIMARY KEY (noSoumission),
-	CONSTRAINT soumissionTrack_fk FOREIGN KEY (idTrack)
-		REFERENCE Track(idTrack)
-
+	CONSTRAINT trackEdition_fk FOREIGN KEY (idEdition) REFERENCES Edition(idEdition),
+	CONSTRAINT nbPapiers_check CHECK (nbPapiersMin <= nbPapiersMax)
 );
 
 CREATE TABLE Chercheur
@@ -99,18 +47,70 @@ CREATE TABLE Chercheur
 	CONSTRAINT chercheur_pk PRIMARY KEY (idChercheur)
 );
 
+CREATE TABLE ComiteRelecture
+(
+	idComiteRelecture NUMBER NOT NULL,
+	idTrack NUMBER NOT NULL,
+	CONSTRAINT idTrack_unique UNIQUE(idTrack),
+	CONSTRAINT comiteRelecture_pk PRIMARY KEY (idComiteRelecture),
+	CONSTRAINT ComiteTrack_fk FOREIGN KEY (idTrack) REFERENCES Track(idTrack)
+);
+
+CREATE TABLE CoPresident
+(
+	idChercheur NUMBER NOT NULL,
+	idComiteRelecture NUMBER NOT NULL,
+	CONSTRAINT copresident_pk PRIMARY KEY (idChercheur, idComiteRelecture),
+	CONSTRAINT chercheur_fk FOREIGN KEY (idChercheur) REFERENCES Chercheur(idChercheur),
+	CONSTRAINT CoPresComiteRelecture_fk FOREIGN KEY (idComiteRelecture) REFERENCES ComiteRelecture(idComiteRelecture)
+);
+
+CREATE TABLE Sujet
+(
+  idSujet int not null,
+	motClef VARCHAR(30) NOT NULL,
+	CONSTRAINT sujet_pk PRIMARY KEY (idSujet),
+	CONSTRAINT sujet_bk UNIQUE (motClef)
+);
+
+CREATE TABLE TrackASujet
+(
+	idTrack NUMBER NOT NULL,
+	motClef VARCHAR(30) NOT NULL,
+	CONSTRAINT TrackASujet_pk PRIMARY KEY (idTrack, motClef),
+	CONSTRAINT TrackASujetTrack_fk FOREIGN KEY (idTrack) REFERENCES Track(idTrack),
+	CONSTRAINT TrackASujetSujet_fk FOREIGN KEY (motClef) REFERENCES Sujet(motClef)
+);
+
+CREATE TABLE Membre
+(
+	idChercheur NUMBER NOT NULL,
+	idComiteRelecture NUMBER NOT NULL,
+	CONSTRAINT membre_pk PRIMARY KEY (idChercheur, idComiteRelecture),
+	CONSTRAINT membreChercheur_fk FOREIGN KEY (idChercheur) REFERENCES Chercheur(idChercheur),
+	CONSTRAINT membreComite_fk FOREIGN KEY (idComiteRelecture) REFERENCES ComiteRelecture(idComiteRelecture)
+);
+
+CREATE TABLE Soumission
+(
+	noSoumission NUMBER NOT NULL,
+	titre VARCHAR(50) NOT NULL,
+	resume VARCHAR(200) NOT NULL,
+	corps BLOB NOT NULL,
+	idTrack NUMBER NOT NULL,
+	CONSTRAINT soumission_pk PRIMARY KEY (noSoumission),
+	CONSTRAINT soumissionTrack_fk FOREIGN KEY (idTrack) REFERENCES Track(idTrack)
+);
+
 CREATE TABLE Evaluation
 (
 	idChercheur NUMBER NOT NULL,
-	idSoumission NUMBER NOT NULL,
+	noSoumission NUMBER NOT NULL,
 	idComiteRelecture NUMBER NOT NULL,
-	CONSTRAINT evalution_pk PRIMARY KEY (idChercheur, idSoumission, idComiteRelecture),
-	CONSTRAINT evalChercheur_fk FOREIGN KEY (idChercheur)
-		REFERENCE Chercheur(idChercheur)
-	CONSTRAINT evalSoumission_fk FOREIGN KEY (idSoumission)
-		REFERENCE Soumission(idSoumission)
-	CONSTRAINT evalComiteRelecture_fk FOREIGN KEY (idComiteRelecture)
-		REFERENCE ComiteRelecture(idComiteRelecture)
+	CONSTRAINT evalution_pk PRIMARY KEY (idChercheur, noSoumission, idComiteRelecture),
+	CONSTRAINT evalChercheur_fk FOREIGN KEY (idChercheur) REFERENCES Chercheur(idChercheur),
+	CONSTRAINT evalSoumission_fk FOREIGN KEY (noSoumission) REFERENCES Soumission(noSoumission),
+	CONSTRAINT evalComiteRelecture_fk FOREIGN KEY (idComiteRelecture) REFERENCES ComiteRelecture(idComiteRelecture)
 );
 
 CREATE TABLE AuteurASoumission
@@ -119,40 +119,24 @@ CREATE TABLE AuteurASoumission
 	idSoumission NUMBER NOT NULL,
 	CONSTRAINT AuteurASoumission_pk PRIMARY KEY (idChercheur, idSoumission),
 	CONSTRAINT soumissionChercheur_fk FOREIGN KEY (idChercheur)
-		REFERENCE Chercheur(idChercheur)
+		REFERENCES Chercheur(idChercheur),
 	CONSTRAINT soumission_fk FOREIGN KEY (idSoumission)
-		REFERENCE Soumission(idSoumission)
+		REFERENCES Soumission(noSoumission)
 );
 
-CREATE TABLE Sujet
-(
-	motClef VARCHAR(30) NOT NULL,
-	CONSTRAINT sujet_pk PRIMARY KEY (motClef),
-);
+create or replace trigger MaxCoPresidentComite
+after insert or update on CoPresident
+for each row
+DECLARE
+compte number;
+BEGIN
+  select count(idChercheur) into compte from CoPresident where idComiteRelecture = :new.idComiteRelecture;
 
-
-CREATE OR REPLACE TRIGGER CheckNbCoPresidentCommite
-	BEFORE INSERT OR UPDATE ON CoPresident
-		FOR EACH ROW
-
-	DECLARE
-	t_count NUMBER;
-	t_coPresident_exception EXCEPTION;
-	PRAGMA EXCEPTION_INIT(t_coPresident_exception, -20001)
-
-	BEGIN
-		SELECT COUNT(idChercheur) INTO t_count	 
-		FROM CoPresident
-		Where idComiteRelecture = :new.idComiteRelecture
-
-		IF( t_count > 2 ) THEN
-			RAISE_APPLICATION_ERROR( -20001, "Pas plus de deux co-president pour un meme track")
-		END IF;
-
-		EXCEPTION
-			WHEN t_coPresident_exception
-			THEN dbms_output.put_line( sqlerrm );
-	END;
+  if compte > 2 THEN
+    RAISE_APPLICATION_ERROR(-20001,'Maximum de Deux co-presidents par track');
+  end if;
+end;
+/
 
 
 CREATE OR REPLACE TRIGGER CheckNbEvaluation
@@ -211,3 +195,20 @@ CREATE OR REPLACE TRIGGER CheckConflitCoPresAuteur
 	END;
 
 
+-- Exercices Demo
+
+create procedure AuteurActif (titreConference as varchar(50)
+as
+avgEditions int;
+begin
+select count(*)/2 into avgEditions from Edition e
+where e.titre = titreConference
+
+select idChercheur from Conference c
+join Edition e on c.titre = e.titre
+join Track t on t.idEdition = e.idEdition
+join Soumission s on s.idTrack = t.idTrack
+join AuteurASoumission a.idSoumission = s.noSoumission
+group by idChercheur, e.idEdition
+where count(insoumission) > avgEditions
+end;
